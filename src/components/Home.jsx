92246868685c8557be2c2cb0edb23f8d6c3f4551
surajkm24 <surajkm336796@gmail.com>
2022-09-12 23:51:58 +0530

    about page added

---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+import { BiDownload } from 'react-icons/bi';
+import '../styles/home.css';
+
+export const Home = () => { 
+    const { light } = useContext(ThemeContext);
+
+    return <div id='home' style={{ '--bglight': light ? "#edf2f8" : "#0a192f" }}>
+        <div id='homegif' >
+            <img src='https://i.giphy.com/media/qgQUggAC3Pfv687qPC/giphy.webp' />
+        </div>
+        <div id='homecontent' style={{ '--plight': light ? 'black' : 'white', '--namelight': light ? "#dc143c" : "#64ffda", '--rolelight': light ? "" : "#dc143c" }}>
+            <p>Hello! Myself</p>
+            <p>Suraj Kumar Mishra</p>
+            <p>I'm a <span>&nbsp;Full Stack Developer.</span></p>
+            <p>An ambitious, self motivated and result driven full stack web developer dedicated to building and optimizing user-focused websites and applications. Able to work under pressure with minimal supervision. A quick learner who can absorb new ideas and can communicate them clearly and effectively.</p>
+            <button>Resume <i>
+               <BiDownload/>
+            </i></button>
+        </div>
+    </div>
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,76 @@
+import { useContext, useEffect, useState } from 'react';
+import { ThemeContext } from '../context/ThemeContext';
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { CgCloseR } from 'react-icons/cg';
+import { FaSun, FaMoon } from 'react-icons/fa';
+import '../styles/navbar.css';
+
+export const Navbar = () => {
+    const [drawer, setDrawer] = useState(false);
+    const { light, setLight } = useContext(ThemeContext)
+
+    const openDrawer = () => {
+        setDrawer(true);
+    }
+
+    const closeDrawer = () => {
+        setTimeout(() => {
+            setDrawer(false)
+        }, 400)
+        if (drawer) {
+            let drawerCont = document.getElementsByClassName('drawerCont')[0]
+            drawerCont.style.transition = '1s linear'
+            drawerCont.style.transform = 'translateX(50vw)'
+        }
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (drawer) {
+                document.getElementsByClassName('drawerCont')[0].style.transition = '1s linear'
+                document.getElementsByClassName('drawerCont')[0].style.transform = 'translateX(-50vw)';
+                //   document.getElementById('drawerCont').style.transition = 'transform 2s linear'
+            }
+        }, -100)
+    }, [drawer])
+
+    return <div className='nav' style={{ background: light ? '#edf2f8' : '#0a192f', '--navTextHover': light ? "#dc143c" : "#64ffda" }}>
+        <div>
+            <svg viewBox="0 0 100 120" style={{ height: "55px", width: "62px" }} >
+                <polygon fill={light ? "#edf2f8" : '#0a192f'} className="Shape" stroke={light ? "#dc143c" : "#64ffda"} stroke-width="5" stroke-linecap="round" stroke-linejoin="round" points="39 2 0 22 0 67 39 90 78 68 78 23"></polygon>
+                <text x='40' y='60' text-anchor="middle" fill={light ? "#dc143c" : "#64ffda"} fontSize="50px" fontWeight="640" >S</text>
+            </svg>
+        </div>
+        <div className='navButtons' style={light ? { '--navText': "black", '--navTextHover': "#dc143c" } : { '--navText': "rgba(255,255,255,0.9)", '--navTextHover': "#dc143c" }}>
+            <p><a href='#'>HOME</a></p>
+            <p><a href='#'>ABOUT</a></p>
+            <p><a href='#'>SKILLS</a></p>
+            <p><a href='#'>PROJECTS</a></p>
+            <p><a href='#'>CONTACT</a></p>
+            <p><a href='#'>RESUME</a></p>
+            {light ? <i className='fas' style={{ cursor: "pointer", fontSize: "20px", color: "#0a192f" }} onClick={() => setLight(false)}><FaMoon /></i> :
+                <i className='fas' style={{ cursor: "pointer", fontSize: "20px", color: "yellow" }} onClick={() => setLight(true)}><FaSun /></i>}
+        </div>
+        <div className='navDrawer' style={light ? { '--navTextHover': "black" } : { '--navTextHover': "#dc143c" }}>
+            <i className="fas" onClick={openDrawer} style={{ color: light ? "black" : "rgba(255,255,255,0.9)", fontSize: "20px", padding: "0px" }}><GiHamburgerMenu size='23px' style={{ padding: "10px 10px" }} /></i>
+            {light ? <i className='fas' style={{ cursor: "pointer", fontSize: "20px", color: "#0a192f" }} onClick={() => setLight(false)}><FaMoon /></i> :
+                <i className='fas' style={{ cursor: "pointer", fontSize: "20px", color: "yellow" }} onClick={() => setLight(true)}><FaSun /></i>}
+        </div>
+
+        <div className='navDrawerBackdrop' style={{ display: drawer ? 'flex' : 'none', '--back1': light ? "#dc143cc5" : "#64ffdb9c", '--back2': light ? "#dc143c90" : "#64ffdd68" }}>
+            <svg viewBox="0 0 100 120" style={{ height: "55px", width: "62px" }} >
+                <polygon fill={light ? "#edf2f8" : '#0a192f'} className="Shape" stroke={light ? "#dc143c" : "#64ffda"} stroke-width="5" stroke-linecap="round" stroke-linejoin="round" points="39 2 0 22 0 67 39 90 78 68 78 23"></polygon>
+                <text x='40' y='60' text-anchor="middle" fill={light ? "#dc143c" : "#64ffda"} fontSize="50px" fontWeight="640" >S</text>
+            </svg>
+            <div className='drawerCont' style={light ? { '--navText': "black", '--navTextHover': "#dc143c", background: "#edf2f8" } : { '--navText': "rgba(255,255,255,0.9)", '--navTextHover': "#dc143c", background: "#0a192f" }}>
+                <i class="fa" onClick={closeDrawer} style={{ color: light ? "black" : "rgba(255,255,255,0.9)", fontSize: "25px", marginLeft: "250px", padding: "0px" }}><CgCloseR style={{ padding: "10px 10px" }} /></i>
+                <p><a href='#'>HOME</a></p>
+                <p><a href='#'>ABOUT</a></p>
+                <p><a href='#'>SKILLS</a></p>
+                <p><a href='#'>PROJECTS</a></p>
+                <p><a href='#'>CONTACT</a></p>
+                <p><a href='#'>RESUME</a></p>
+            </div>
+        </div>
+    </div>
+}
