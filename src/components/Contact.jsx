7bf42e28a9ae644c061d4d0c0018c5c8d9a9668d
surajkm24 <surajkm336796@gmail.com>
2022-09-18 23:23:25 +0530

    github stat

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,74 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import '../styles/contact.css';
 import { ThemeContext } from '../context/ThemeContext';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
+import { MdEmail } from 'react-icons/md'
+import { useRef } from 'react';
+import {RiContactsFill} from 'react-icons/ri';
+
 export const Contact = () => {
     const { light } = useContext(ThemeContext);
+    const [formData, setFormData] = useState({ from_name: "", email_id: "", message: "" });
+    const [msgStatus, setStatus] = useState({ fail: false, suc: false })
+    const form = useRef();
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        if (formData.name === "" || formData.email === "" || formData.message === "") {
+            setStatus({ ...msgStatus, fail: true, suc: false });
+
+            setTimeout(() => {
+                setStatus({ ...msgStatus, fail: false, suc: false });
+            }, 3000)
+        }
+        else {
+            emailjs.sendForm('service_bl30v2n', 'template_nj6lzpu', form.current, '_dqgPiGtLggjDM6ew')
+                .then((result) => {
+                    console.log(result.text);
+                    setStatus({ ...msgStatus, fail: false, suc: true });
+                    setTimeout(() => {
+                        setStatus({ ...msgStatus, fail: false, suc: false });
+                    }, 3000)
+
+                    setFormData({ from_name: "", email_id: "", message: "" });
+                }, (error) => {
+                    console.log(error.text);
+                });
+        }
+
+    }
+
     return <div id='contact' style={{ '--bglightproject': light ? "#edf2f8" : "#0a192f", '--plight': light ? 'black' : 'white', '--namelight': light ? "#dc143c" : "#64ffda", '--rolelight': light ? "white" : "#152235" }}>
-        <p>Get In <span>Touch</span> <span></span></p>
+        <p>Get In <span>Touch</span> <span></span><RiContactsFill/></p>
         <div id='contactCont'>
             <div id='contImg'>
-                <img src='https://raw.githubusercontent.com/surajkm24/surajkm24.github.io/2721d70cd4216cb14df8c2fc62bb172d632ab47e/assets/contact_me.svg' />
             </div>
-            <div id='contForm'></div>
+            <div id='contForm'>
+                <div id='smButt'>
+                    <a href='https://www.linkedin.com/in/surajkm24/' target='_blank' style={{ textDecoration: "none" }}><button className='smButIc' style={{ background: light ? "#00a0dc" : "#9bdaf3", color: light ? "white" : "black" }}><BsLinkedin /> LinkedIn</button></a>
+                    <a href='https://github.com/surajkm24' target='_blank' style={{ textDecoration: "none" }}><button className='smButIc' style={{ background: light ? "#373b41" : "#373b41", color: "white" }}><BsGithub /> GitHub</button></a>
+                    <a href='mailto:surajkm336796@gmail.com' style={{ textDecoration: "none" }}><button className='smButIc' style={{ background: light ? "#e53e3e" : "#feb2b2", color: light ? "white" : "black" }}><MdEmail /> Gmail</button></a>
+                </div>
+                <div id='contForm2'>
+                    <form ref={form} onSubmit={sendMessage}>
+                        <input name='from_name' type='text' placeholder='Your Name' value={formData.from_name} onChange={(e) => setFormData({ ...formData, from_name: e.target.value })} />
+                        <input name='email_id' type='email' placeholder='Your Email' value={formData.email_id} onChange={(e) => setFormData({ ...formData, email_id: e.target.value })} />
+                        <textarea name='message' type='text' placeholder='Your Message' value={formData.message} onChange={(e) => setFormData({ ...formData, message: e.target.value })} />
+                        <input type='submit' value='SEND' />
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div id='failbox' style={{ display: msgStatus.fail ? "flex" : "none" }}>
+            <div id='failureAlert'>
+                <span>!</span> <p>Please fill the details</p>
+            </div>
+        </div>
+        <div id='sucbox' style={{ display: msgStatus.suc ? "flex" : "none" }}>
+            <div id='successAlert'>
+                <span>✓</span> <p>Your message has been sent.</p>
+            </div>
         </div>
     </div>
 }
--- src/components/GithubStats.jsx
+++ src/components/GithubStats.jsx
@@ -2,10 +2,33 @@ import { useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 import '../styles/githubstats.css';
 import { SiGithub } from 'react-icons/si';
+import { useEffect } from 'react';
+import { useRef } from 'react';
 
 export const GithubStats = () => {
     const { light } = useContext(ThemeContext);
-    return <div id='githubstats' style={{ '--bglightstats': light ? "#edf2f8" : "#0a192f", '--bglight2stats': light ? "white" : "#152235", '--plight': light ? 'black' : 'white', '--namelight': light ? "#dc143c" : "#64ffda", '--rolelight': light ? "" : "#dc143c" }}>
-        <p>Git<span>Hub</span> Stats <SiGithub id='gitStIc' fontSize={'25px'} style={{transition:"none",'--icColor':light?"black":"white"}} /></p>
+    const calender = useRef()
+    useEffect(()=>{
+        setTimeout(()=>{
+            GitHubCalendar(calender.current, "surajkm24", { responsive: true });
+        },10000)
+    },[])
+    return <div id='githubstats' style={{ '--bglightstats': light ? "#edf2f8" : "#0a192f", '--bglight2stats': light ? "white" : "#152235", '--plight': light ? 'black' : 'white', '--namelight': light ? "#dc143c" : "#64ffda", '--rolelight': light ? "#edf2f8" : "#273849" }}>
+        <p>Sta<span>tis</span>tics <SiGithub id='gitStIc' fontSize={'25px'} style={{ transition: "none", '--icColor': light ? "black" : "white" }} /></p>
+        <div id='statCont'>
+            <div>
+                {!light ? <img src='https://github-readme-stats.vercel.app/api?username=surajkm24&theme=vue-dark&show_icons=true&hide_border=true&count_private=true' />
+                    : <img src='https://github-readme-stats.vercel.app/api?username=surajkm24&theme=flag-india&show_icons=true&hide_border=true&count_private=true&title_color=blue&text_color=black&bg_color=edf2f8' />
+                }
+            </div>
+            <div>
+                {light ? <img src='https://github-readme-stats.vercel.app/api/top-langs/?username=surajkm24&theme=flag-india&show_icons=true&hide_border=true&layout=compact&title_color=blue&text_color=black&bg_color=edf2f8' />
+                    : <img src='https://github-readme-stats.vercel.app/api/top-langs/?username=surajkm24&theme=vue-dark&show_icons=true&hide_border=true&layout=compact' />
+                }
+            </div>
+        </div>
+        <div className='calender' ref={calender}>
+
+        </div>
     </div>
 }
