08c600f6aa2b397bf5b6b651c3813bde99dc95a1
surajkm24 <surajkm336796@gmail.com>
2022-10-19 17:36:59 +0530

    project updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,13 +3,42 @@ import '../styles/projects.css';
 import { ThemeContext } from '../context/ThemeContext'
 import { GoProject } from 'react-icons/go'
 import { FaEye } from 'react-icons/fa'
-import { SiChakraui, SiGithub, SiHtml5, SiCss3, SiJavascript, SiReact, SiReactrouter, SiHeroku, SiRedux, SiVercel, SiExpress, SiMongodb, SiGit, SiNetlify } from 'react-icons/si'
+import { SiChakraui,SiNodedotjs, SiGithub, SiHtml5, SiCss3, SiJavascript, SiReact, SiReactrouter, SiHeroku, SiRedux, SiVercel, SiExpress, SiMongodb, SiGit, SiNetlify } from 'react-icons/si'
 
 export const Projects = () => {
     const { light } = useContext(ThemeContext);
 
     return <div id='projects' style={{ '--bglightproject': light ? "#edf2f8" : "#0a192f", '--plight': light ? 'black' : 'white', '--namelight': light ? "#dc143c" : "#64ffda", '--rolelight': light ? "white" : "#152235" }}>
         <p>Featured <span>Projects </span> <span><GoProject /></span></p>
+        <div className='projectCard'>
+            <div className='prCardImg'>
+                <img src='https://raw.githubusercontent.com/surajkm24/surajkm24.github.io/master/assets/Screenshot%20(667).png' />
+            </div>
+            <div className='prCardContent'>
+                <p>TrackingTime.co - Clone</p>
+                <p>This project is about building a full stack clone of
+                    TrackingTime.co, which is a team-oriented time tracking and project management software 
+                    product. Users can signup, login, create projects, add tasks, update tasks, track time 
+                    and much more. It is built using some front-end and back-end technologies listed below.
+                    <p><span>Tech Stack: </span>React, ChakraUI, CSS, React-Router, Redux, NodeJS, ExpressJS,
+                    MongoDB.</p>
+                </p>
+                <div className='prCardTs'>
+                    <SiReact style={{ color: "#53c1de" }} className='tsIc' />
+                    <SiChakraui style={{ color: "#60c9ca", background: "white", borderRadius: "50%" }} className='tsIc' />
+                    <SiCss3 style={{ color: "#1c88c7" }} className='tsIc' />
+                    <SiReactrouter style={{ color: "tomato" }} className='tsIc' />
+                    <SiRedux style={{ color: "purple" }} className='tsIc' />
+                    <SiNodedotjs style={{ color: "green" }} className='tsIc' />
+                    <SiExpress style={{ color: "black" }} className='tsIc' />
+                    <SiMongodb style={{ color: "green" }} className='tsIc' />
+                </div>
+                <div style={{ '--icBg': light ? "transparent" : "#0a192f" }}>
+                    <a style={{ textDecoration: "none" }} href='https://wondrous-stardust-501a11.netlify.app/' target='_blank'><button><FaEye className='tsBIc' /> Go live</button></a>
+                    <a style={{ textDecoration: "none" }} href='https://github.com/surajkm24/trackingtime.co-clone' target='_blank'><button> <SiGithub className='tsBIc' /> View code</button></a>
+                </div>
+            </div>
+        </div>
         <div className='projectCard'>
             <div className='prCardImg'>
                 <img src='https://raw.githubusercontent.com/surajkm24/surajkm24.github.io/master/assets/Screenshot%20(667).png' />
