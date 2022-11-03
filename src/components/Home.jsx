bb2c452c7a535d19295594ac4657aca8e763004e
surajkm24 <surajkm336796@gmail.com>
2022-11-03 17:27:02 +0530

    added tag cloud in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,36 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 import { BiDownload } from 'react-icons/bi';
 import '../styles/home.css';
-import { BsLinkedin, BsGithub} from 'react-icons/bs';
-import {SiGmail} from 'react-icons/si'
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
+import { SiGmail } from 'react-icons/si';
+import TagCloud from 'TagCloud'
+
+const myTags = [
+    'JavaScript', 'CSS', 'HTML',
+    'Redux', 'React Router', 'React',
+    'TypeScript', 'GIT', 'MongoDB',
+    'Express', 'NodeJs', 'Mongoose',
+    'ChakraUI', 'Netlify', 'Jest', 'Babel'
+];
 
 export const Home = () => {
     const { light } = useContext(ThemeContext);
 
+    useEffect(() => {
+        TagCloud('.content', myTags, {
+            radius: 200,
+            maxSpeed: 'fast',
+            initSpeed: 'fast',
+            direction: 135,
+            keep: true
+        })
+    }, [])
+
     return <div id='home' style={{ '--bglight': light ? "#edf2f8" : "#0a192f" }}>
-        <div id='homegif' >
-            <img src='https://i.giphy.com/media/qgQUggAC3Pfv687qPC/giphy.webp' />
-        </div>
+        {/* <div id='homegif' >
+            <img src='https://www.wingstechsolutions.com/wp-content/uploads/2022/03/full-stack-development.gif' />
+        </div> */}
         <div id='homecontent' style={{ '--plight': light ? 'black' : 'white', '--namelight': light ? "#dc143c" : "#64ffda", '--rolelight': light ? "" : "#dc143c" }}>
             <p>Hi, I am</p>
             <p>Suraj Kumar Mishra.</p>
@@ -23,7 +42,7 @@ export const Home = () => {
                         <BiDownload />
                     </i></button>
                 </a>
-                <div className='homeIcCont' style={{'--icColor':light?"#0a192f":"#edf2f8"}}>
+                <div className='homeIcCont' style={{ '--icColor': light ? "#0a192f" : "#edf2f8" }}>
                     <div>
                         <a href='https://linkedin.com/in/surajkm24' target='_blank'>
                             <BsLinkedin className='homeIc' />
@@ -40,6 +59,11 @@ export const Home = () => {
                         </a>
                     </div>
                 </div>
+            </div>
+        </div>
+        <div>
+            <div className="content" style={{ order: 1, '--color': light ? "#dc143c" : "#64ffda" }}>
+
             </div>
         </div>
     </div>
